67e6eb6574d3c82d471f259939cebf7ea9ea8077
atkunja <atkunjadia@gmail.com>
2026-09-19 03:53:54 -0400

    refactor: retire the driving world and legacy interface

--- sources/data/social.js
+++
@@ -1,5 +0,0 @@
-export default [
-    { name: 'GitHub', url: 'https://github.com/atkunja', align: 'right' },
-    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/ayushkunjadia/', align: 'right' },
-    { name: 'Mail', url: 'mailto:ayushkun@umich.edu', align: 'left' },
-]
